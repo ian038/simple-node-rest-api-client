4997c63adb60cb0fd30e6a2b3ead1acfd021c2c2
ian038 <ianphua3@gmail.com>
2022-05-04 13:29:40 -0400

    I added /read endpoint to list out all spartans

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ app.get('/', function (req, res) {
     res.send('Hello world')
 })
 
+app.get('/read', function (req, res) {
+    fs.readFile(__dirname + "/" + "spartans.json", 'utf8', function (err, data) {
+        console.log(data);
+        res.end(data);
+    });
+})
+
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
