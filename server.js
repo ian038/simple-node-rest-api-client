239f73eeecee82d9011a805d76a20cbca8dc9178
ian038 <ianphua3@gmail.com>
2022-05-04 13:44:23 -0400

    I added create endpoint which accepts and logs a json object, I also edited spartans.json file to conform to json syntax

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const fs = require("fs");
 
+app.use(express.json())
+
 app.get('/', function (req, res) {
     res.send('Hello world')
 })
@@ -11,6 +13,11 @@ app.get('/read', function (req, res) {
         console.log(data);
         res.end(data);
     });
+})
+
+app.post('/create', function (req, res) {
+    console.log(req.body)
+    res.json(req.body)
 })
 
 const port = process.env.PORT || 5000
